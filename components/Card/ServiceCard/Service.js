47ca896ac9a2aae093b60c141e9cd49fd986baba
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-03 15:53:23 +0200

    update

--- components/Card/ServiceCard/Service.js
+++ components/Card/ServiceCard/Service.js
@@ -5,21 +5,25 @@ import Button from '../../Button/Button';
 export default function Service({ title, cardData }) {
   return (
     <section className='bg-hero-pattern bg-secondary-bg mt-6 pt-44 pb-28 md:w-full'>
-      <div className='grid md:grid-cols-[300px_200px_250px_250px] mx-auto container gap-[55px]'>
-        <div className='md:col-span-2 md:row-span-3 lg:mx-16'>
-          <h1 className='text-white text-[54px] font-primary-font font-bold text-start -mt-5'>
-            {title}
-          </h1>
-        </div>
-        {cardData.map((card) => {
-          return (
-            <div key={card.id} className=''>
-              <CardService {...card} />
+      <div className='mx-auto container'>
+        <div className='flex w-[1120px] h-[803px] gap-[100px] lg:mx-16'>
+          <div className=' w-[352px] h-[730px]'>
+            <h1 className='text-white text-[54px] font-primary-font font-bold text-start -mt-5'>
+              {title}
+            </h1>
+          </div>
+          <div className='grid grid-cols-2 w-[640px] h-[664px] gap-[100px]'>
+            {cardData.map((card) => {
+              return (
+                <div key={card.id} className='lg:-mb-10'>
+                  <CardService {...card} />
+                </div>
+              );
+            })}
+            <div className=''>
+              <Button title='See our services' />
             </div>
-          );
-        })}
-        <div className='py-8'>
-          <Button title='See our services' />
+          </div>
         </div>
       </div>
     </section>
