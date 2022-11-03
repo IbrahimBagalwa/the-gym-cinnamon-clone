ac5619fcd0c4c053624be5e5c4ece56c4c6e9b09
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-03 18:57:41 +0200

    update

--- components/Card/Team/College.jsx
+++ components/Card/Team/College.jsx
@@ -7,18 +7,24 @@ export default function CollegeTeam({ title, description, image }) {
     <section className=' w-full'>
       <div className=' container mx-auto'>
         <div
-          className={`flex w-[1120px] h-[192px] items-center  gap-[200px] mx-16 pt-52 mt-20`}
+          className={`flex lg:w-[1120px] lg:h-[192px] lg:items-center md:flex-col lg:flex-row lg:gap-[200px] lg:mx-16 mx-8 lg:pt-52 lg:mt-20 md:py-20`}
         >
-          <Image width='300' height='198' src={title} alt='title' />
+          <Image
+            width='300'
+            height='198'
+            src={title}
+            alt='title'
+            className='md:mb-10'
+          />
           <div>
-            <p className='text-base font-primary-font w-[352px] text-card-paragrah h-[104px] mb-4'>
+            <p className='text-base font-primary-font lg:w-[352px] text-card-paragrah lg:h-[104px] lg:mb-4 mb-8'>
               {description}
             </p>
             <Button title='See How It Works' />
           </div>
         </div>
       </div>
-      <div className='flex justify-end'>
+      <div className='lg:flex hidden justify-end'>
         <Image
           src={image}
           width='556'
